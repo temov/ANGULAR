e94358effbe7029286c9a6385dfa754d162ede71
Tode Temov <75773817+temov@users.noreply.github.com>
2023-08-15 02:09:49 +0200

    Add files via upload

--- Rent-a-car_app/src/app/app.module.ts
+++ Rent-a-car_app/src/app/app.module.ts
@@ -4,15 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CarsListComponent } from './components/cars-list/cars-list.component';
 import { FilterOptionsComponent } from './components/filter-options/filter-options.component';
+import { FormsModule } from '@angular/forms';
+import { CarSortPipe } from './pipes/car-sort.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     CarsListComponent,
-    FilterOptionsComponent
+    FilterOptionsComponent,
+    CarSortPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
