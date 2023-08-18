e55d6eb31382e97b10fbd7f8c04989cce02fa339
Tode Temov <75773817+temov@users.noreply.github.com>
2023-08-18 15:44:33 +0200

    Add files via upload

--- Rent-a-car_app- services/src/app/components/filter-options/filter-options.component.ts
+++ Rent-a-car_app- services/src/app/components/filter-options/filter-options.component.ts
@@ -1,12 +1,18 @@
 import { Component,Output,EventEmitter } from '@angular/core';
 import { Car } from 'src/app/interfaces/car.interface';
 
+import { CarRentService } from 'src/app/services/car-rent.service';
+
 @Component({
   selector: 'app-filter-options',
   templateUrl: './filter-options.component.html',
   styleUrls: ['./filter-options.component.scss']
 })
 export class FilterOptionsComponent {
+
+  constructor(
+    private readonly carRentService: CarRentService
+  ) {}
   @Output() filterSelected = new EventEmitter<string>();
 
   applyFilter(filter: string) {
