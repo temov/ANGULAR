257c786130dce9a6b16bd1818193c253fc96de0c
Tode Temov <75773817+temov@users.noreply.github.com>
2023-08-20 15:31:53 +0200

    Add files via upload

--- Rent-a-car_app- services/src/app/components/cars-list/cars-list.component.ts
+++ Rent-a-car_app- services/src/app/components/cars-list/cars-list.component.ts
@@ -10,36 +10,47 @@ import { CarRentService } from 'src/app/services/car-rent.service';
 export class CarsListComponent implements OnInit {
   
       cars_list : Car[]= [];
-      filteredCars : Car[]= [];
-      showAllCars: boolean = true;
-      sortDirection: 'asc' | 'desc' = 'asc';
+      filteredCars:Car[] = [];
+      showAllCars:boolean = true;
+      sortDirection:"asc" | "desc" = "asc"
 
       constructor(
-        private readonly carRentService: CarRentService
+        private readonly carRentService: CarRentService,
       ) {}
 
   ngOnInit(): void {
     
 
     this.cars_list = this.carRentService.getCars();
+    this.showAllCars = this.carRentService.showAllCars;
+
   }
 
-  toggleSort(): void {
-    this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
-  }
-
-  toggleRented(car:Car):void {
-    this.carRentService.toggleRented(car)
-
+  ngDoCheck(): void {
     
-}
 
-  
+    this.filteredCars = this.carRentService.filteredCars;
+    this.showAllCars = this.carRentService.showAllCars;
+
+  }
+
+      toggleSort(): void {
+        this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+      }
+
+      toggleRented(car:Car):void {
+        this.carRentService.toggleRented(car)
+
+        
+    }
+      filterCars(filter:string):void{
+
+      this.carRentService.filterCars(filter)
+    }
 }
  
 
   
-
 
 
 
